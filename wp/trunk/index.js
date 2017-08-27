a1beabae767ad0d71ac3c23f3a13738efd2b79c9
ivictbor <ivictbor@gmail.com>
2017-08-27 16:29:28 +0300

    0.2.38

--- wp/trunk/index.js
+++ wp/trunk/index.js
@@ -90,7 +90,7 @@
         return {
             longname: "Painterro",
             author: "Ivan Borshchov",
-            version: "0.2.37",
+            version: "0.2.38",
         };
     }
   });
