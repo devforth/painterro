0a2c5d8276da1bd36404cc0659032c694db95cbe
ivictbor <iborschov@agilevision.io>
2018-01-20 12:58:06 +0200

    0.2.56

--- wp/trunk/index.js
+++ wp/trunk/index.js
@@ -90,7 +90,7 @@
         return {
             longname: "Painterro",
             author: "Ivan Borshchov",
-            version: "0.2.55",
+            version: "0.2.56",
         };
     }
   });
