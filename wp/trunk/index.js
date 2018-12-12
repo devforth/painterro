ecd80603992b0f8febf29a096bb980376aa17b92
Ivan Borshchov <ivictbor@gmail.com>
2018-12-12 09:18:51 +0200

    0.2.70

--- wp/trunk/index.js
+++ wp/trunk/index.js
@@ -90,7 +90,7 @@
         return {
             longname: "Painterro",
             author: "Ivan Borshchov",
-            version: "0.2.69",
+            version: "0.2.70",
         };
     }
   });
