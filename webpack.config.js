d6c1b6934f37c13245c61402ae1e9810d5ab3a2b
Ivan Borshchov <ivictbor@gmail.com>
2019-08-11 12:46:52 +0300

    0.2.77

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ function webpackConfig(target) {
       libraryTarget: target
     },
     module: {
-      loaders: [
+      rules: [
         {
           enforce: "pre",
           test: /\.js$/,
