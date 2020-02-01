076f320db92bdfb2f9778ecf3fa1a70608fed899
Ivan Borshchov <ivictbor@gmail.com>
2020-02-01 21:43:50 +0200

    1.0.35

--- wp/trunk/index.js
+++ wp/trunk/index.js
@@ -90,7 +90,7 @@
         return {
             longname: "Painterro",
             author: "Ivan Borshchov",
-            version: "1.0.34",
+            version: "1.0.35",
         };
     }
   });
