9473e0056fb9d31c7bbd60c9a720a7283ad96f18
Ivan Borshchov <ivictbor@gmail.com>
2020-10-27 21:55:59 +0200

    1.1.7

--- wp/trunk/painterro-wp.php
+++ wp/trunk/painterro-wp.php
@@ -4,12 +4,12 @@
 Plugin Name: Painterro
 Plugin URI: https://github.com/ivictbor/painterro
 Description: Paste screenshots and edit images directly in browser. Adds Painterro button to visual editor for images editing. Absolutely free and open source.
-Version: 1.1.6
+Version: 1.1.7
 Author: Ivan Borshchov
 License: MIT
 */
 
-define("PAINTERRO_FILE", "painterro-1.1.6.min.js");
+define("PAINTERRO_FILE", "painterro-1.1.7.min.js");
 
 function include_painterro_script()
 {
