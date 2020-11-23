5090db8e8a451776237f1bc184c1feda0cab7180
Ivan Borshchov <ivan@devforth.io>
2020-11-23 15:55:44 +0200

    1.2.10

--- wp/trunk/index.js
+++ wp/trunk/index.js
@@ -90,7 +90,7 @@
         return {
             longname: "Painterro",
             author: "Ivan Borshchov",
-            version: "1.2.9",
+            version: "1.2.10",
         };
     }
   });
